4f53d1f455343a1f273407c28b4b1f91c28447f6
Jameel <jkelley@iastate.edu>
2017-10-20 22:12:59 -0500

    Title & Credits

---
+++ All is Well/Assets/_scripts/GameManagerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public void loadCredits() {
+		SceneManager.LoadScene ("CreditsScene");
+	}
+
+	public void startGame() {
+		SceneManager.LoadScene ("TitleScene");
+	}
+
+	public void loadTitle() {
+		SceneManager.LoadScene ("TitleScene");
+	}
+}
